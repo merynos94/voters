a75df16a8a245190837927673e1bb5d2ccea759f
Radoslaw Mikos <merynos94@gmail.com>
2020-05-21 15:07:44 +0200

    Przygotowanie site-menu.

--- frontend/src/components/site-menu/site-menu.jsx
+++ frontend/src/components/site-menu/site-menu.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import './site-menu.scss';
+import './site-menu.css';
 
 
-function SiteMenu() {
+export const SiteMenu = () => {
     return (
-<div class="menu">
-<a href="#section1">section1</a>
-<a href="#section2">section2</a>
-<a href="#section3">section3</a>
+        <div className='background-space'>
+            <div className='menu-space'>
+            {/* <div className='x-symbole'><p>x</p></div> */}
+            <button class="header-menu-button__button active" type="button" title="Open menu" aria-expanded="true" aria-pressed="true" >
+            <span class="header-menu-button__bars"></span><span class="header-menu-button__text">Menu</span></button>
+
+            </div>
+
+            <div className='section-space'>
+            <div className='main-menu-container'>
+         
+
+            <ul id="menu-main-navigation" className="main-menu__container">
+            <li className="menu-item">
+            <a href="#home">Home</a></li>
+            
+            <li className="menu-item">
+            <a href="#home">About</a></li>
+            
+            <li className="menu-item">
+            <a href="#home">FAQ</a></li>
+            
+            <li className="menu-item">
+            <a href="#home">Contact</a></li>
+            </ul>
+            </div>
+            </div>
 </div>
-    );
+    )
 }
