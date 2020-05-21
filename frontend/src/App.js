aec86f1b07c085c5be35a879dd88e697c12856af
Radoslaw Mikos <merynos94@gmail.com>
2020-05-21 23:23:00 +0200

    poprawione svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import SiteMenu from './components/site-menu/site-menu';
 //import LoginMenu from './components/login-menu/login-menu';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component.jsx';
+// import Header from './components/header/header.component.jsx';
 
 function App() {
   
