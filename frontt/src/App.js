afc92a85e446f7f33f1de8991903d193d871bd37
Radoslaw Mikos <merynos94@gmail.com>
2020-06-11 00:38:34 +0200

    Dodanie route i komponentów

---
+++ frontt/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router,Route, Redirect, Switch} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return( 
+    <Router>
+    <Route path="/" exact >
+      <Users />
+    </Route>
+
+    <Route path="/places/new" exact >
+      <NewPlace />
+      </Route>
+       
+      <Redirect to="/" />
+  </Router>
+  );
+};
+
+export default App;
