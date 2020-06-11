b6ad3094b40058dbd8c97c77a42d9deb23be4019
Radoslaw Mikos <merynos94@gmail.com>
2020-06-11 23:26:13 +0200

    Poprawka - boczne menu.

--- frontt/src/shared/components/Navigation/MainNavigation.js
+++ frontt/src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../../../shared/components/UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-    <React.Fragment>
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+    
+    const openDrawer = () =>{
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () =>{
+        setDrawerIsOpen(false);
+    };
     return (
+        <React.Fragment>
+    {drawerIsOpen && <Backdrop  onClick={closeDrawer} />}
+    {drawerIsOpen && (
     <SideDrawer>
-        <nav className="main-navigation__draver-nav"></nav>
+        <nav className="main-navigation__draver-nav">
         <NavLinks />
-    </SideDrawer>
+        </nav>
+    </SideDrawer> 
+    )}
     <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
             <span />
             <span />
             <span />
@@ -28,6 +42,7 @@ const MainNavigation = props => {
         </nav>
     </MainHeader>
     </React.Fragment>
+    );
 };
 
 export default MainNavigation;
