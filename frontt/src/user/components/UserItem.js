16bd614db77b625af1134b371f3fe197392c18a0
Radoslaw Mikos <merynos94@gmail.com>
2020-06-11 20:54:07 +0200

    Dodanie komponentu Navi Menu i Hamburger Menu.

--- frontt/src/user/components/UserItem.js
+++ frontt/src/user/components/UserItem.js
@@ -8,11 +8,11 @@ import './UserItem.css';
 
 const UserItem = props => {
   return (
-    <li className="user-item"> 
-        <Card className="user-item__content" >
-        <Link to ={`/${props.id}/places`}>
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-           <Avatar image = {props.image} alt={props.name} />
+            <Avatar image={props.image} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
@@ -20,11 +20,10 @@ const UserItem = props => {
               {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
             </h3>
           </div>
-          </Link>
-          </Card>
+        </Link>
+      </Card>
     </li>
-
-);
+  );
 };
 
 export default UserItem;
--- frontt/src/user/components/UsersList.js
+++ frontt/src/user/components/UsersList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card/Card';
 
 import './UsersList.css';
 
@@ -8,9 +9,9 @@ const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        
+        <Card>
           <h2>No users found.</h2>
-       
+          </Card>
       </div>
     );
   }
