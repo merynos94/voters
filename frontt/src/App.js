c811faeaf6eaafceb63d07c784e0d0a7c21d55fe
Radoslaw Mikos <merynos94@gmail.com>
2020-06-14 00:46:58 +0200

    Dodanie Panelu Logowania.

--- frontt/src/App.js
+++ frontt/src/App.js
@@ -10,6 +10,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 
@@ -31,6 +32,10 @@ const App = () => {
         
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
+
           </Route>
         <Redirect to="/" />
       </Switch>
