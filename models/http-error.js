630404098af5b1104f81025fe16e8a3c462a6e57
Radoslaw Mikos <merynos94@gmail.com>
2020-06-14 17:36:12 +0200

    Stworzenie swojego własnego modelu błędu strony.

---
+++ models/http-error.js
@@ -0,0 +1,11 @@
+class HTTPError extends Error {
+    constructor(message, errorCode) {
+        super(message);
+        this.code = errorCode;
+
+
+
+    }
+}
+
+module.exports = HTTPError;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
 const router = express.Router();
 
 
@@ -25,10 +26,9 @@ router.get('/:pid', (req,res,next) => {
     });
     
     if (!place) {
-      const error = new Error ('Could not find a place for the provided id.');
-      error.code = 404;
-      throw error;
-    }
+      throw new HtttpError ('Could not find a place for the provided id.', 404  );
+     
+         }
 
     res.json({place});
 });
@@ -40,9 +40,9 @@ router.get('/user/:uid', (req,res,next) => {
         return p.creator === userId;
 });
 if (!place) {
-    const error = new Error ('Could not find a place for the provided user id.');
-    error.code = 404;
-return next( error);
+    return next(
+        new HttpError('Could not find a place for the provided user id', 404)
+    );
   }
 
     res.json({place});
