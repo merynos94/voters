db0dd9ca3ccad5dd813c6b6d98aca3a1a41e3001
Radoslaw Mikos <merynos94@gmail.com>
2020-06-14 17:29:07 +0200

    Dodanie zgłaszanie błędów serwera.

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -24,7 +24,27 @@ router.get('/:pid', (req,res,next) => {
         return p.id === placeId;
     });
     
+    if (!place) {
+      const error = new Error ('Could not find a place for the provided id.');
+      error.code = 404;
+      throw error;
+    }
+
     res.json({place});
 });
 
+router.get('/user/:uid', (req,res,next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p=> {
+        return p.creator === userId;
+});
+if (!place) {
+    const error = new Error ('Could not find a place for the provided user id.');
+    error.code = 404;
+return next( error);
+  }
+
+    res.json({place});
+});
 module.exports = router;
