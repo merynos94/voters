268b3d509fd88fd80dbf1155d09d23bab7095635
Radoslaw Mikos <merynos94@gmail.com>
2020-06-14 13:18:14 +0200

    Dodanie buttona logout, poprawka menu.

--- frontt/src/shared/components/Navigation/MainNavigation.js
+++ frontt/src/shared/components/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ const MainNavigation = props => {
                     <span />
                     <span />
                 </button>
-                <h1 className="main=navigation__title">
+                <h1 className="main-navigation__title">
                     <Link to="/">YourPlaces</Link>
                 </h1>
                 <nav className="main-navigation__header-nav">
