573cc243ca1e0160df59b4bea476ae6b815395d1
Radoslaw Mikos <merynos94@gmail.com>
2020-06-14 16:35:11 +0200

    Backend - Ustawienie podstawowego routingu.

--- frontend/src/components/slide-menu/MenuItem.jsx
+++
@@ -1,9 +0,0 @@
-var MenuItem = React.createClass({
-    navigate: function(hash) {
-      window.location.hash = hash;
-    },
-  
-    render: function() {
-      return <div className="menu-item" onClick={this.navigate.bind(this, this.props.hash)}>{this.props.children}</div>;
-    }
-  });
