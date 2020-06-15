bb5bc5ee017e7e4810baa56e85b7a6b6a896af08
Radoslaw Mikos <merynos94@gmail.com>
2020-06-15 23:24:30 +0200

    Bazy danych - MongoDb. Inicjalizacja środowiska.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -24,5 +25,11 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500)
   res.json({message: error.message || 'An unknown error occurred!'});
 });
-
-app.listen(5000);
+mongoose
+.connect('mongodb+srv://radek:radek1234@cluster0-x4rnu.mongodb.net/<places>?retryWrites=true&w=majority')
+.then(() =>{
+  app.listen(5000);
+})
+.catch(err => {
+  console.log(err);
+});
