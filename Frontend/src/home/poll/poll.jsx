a72afc3233e98c5fcca47a910bc3dff1f8251132
Beniamin Bartkowiak <beniamin.bartkowiak@boardintelligence.co.uk>
2020-06-19 02:01:45 +0200

    voting is possible now

--- Frontend/src/home/poll/poll.jsx
+++ Frontend/src/home/poll/poll.jsx
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
-import { useForm } from '../../shared/hooks/form-hooks';
+import { useForm } from 'react-hook-form';
 
 const voteUri = 'http://localhost:3000/votes';
 const candidatesUri = 'http://localhost:3060/candidates'
 
-const candidateInput = (candidate) => {
-  <Input
-    element="input"
-    id={ candidate.id }
-    type="radio"
-    label={ candidate.description }
-  />
-};
+const candidateInput = (candidate) => (
+  <p key={ candidate.id }>
+    <input id={ candidate.id } type="radio" name="candidate" value={ candidate.uuid } />
+    <label htmlFor={ candidate.id }>{ candidate.description }</label>
+  </p>
+);
 
-const formSubmit = async event => {
-  event.preventDefault();
+const formSubmit = (data) => {
+  const body = JSON.stringify({
+    data: {
+      id: data.voterId,
+      message: { candidate: data.candidate },
+    }
+  })
 
+  fetch(voteUri, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body })
+    .then(res => console.log("vote success! ", res.json()))
+    .catch(err => console.log("uh oh! ", err))
 };
 
 const Poll = props => {
+  const { handleSubmit, register } = useForm();
   const [candidates, setCandidates] = useState({});
 
   useEffect(
@@ -32,15 +39,27 @@ const Poll = props => {
     []
   )
 
-
-
   return (
-    <form onSubmit={ formSubmit }>
-      { _.map(candidates, candidate => candidateInput(candidate)) }
-      <Button type="submit" disabled={!formState.isValid}>
-      </Button>
+    <form onSubmit={ handleSubmit(formSubmit) }>
+      {
+        _.map(
+          candidates,
+          candidate => (
+            <p key={ candidate.id }>
+              <input ref={ register } id={ candidate.id } type="radio" name="candidate" value={ candidate.id } />
+              <label htmlFor={ candidate.id }>{ candidate.description }</label>
+            </p>
+          )
+        )
+      }
+      <p>
+        <input ref={ register } type="text" id="voterId" name="voterId" />
+        <label htmlFor="key">Your voter ID</label>
+      </p>
+      <input type="submit">
+      </input>
     </form>
   );
 };
 
-export default ResultsList;
+export default Poll;
