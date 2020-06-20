fef74c8b36d1d8791f1bb2e024c795122a3c8af9
Radoslaw Mikos <merynos94@gmail.com>
2020-06-21 01:33:05 +0200

    uwierzytelnienie.

--- Backend/controllers/users-controllers.js
+++ Backend/controllers/users-controllers.js
@@ -1,4 +1,7 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
@@ -15,8 +18,8 @@ const getUsers = async (req, res, next) => {
   }
   res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
-const signup = async (req, res, next) => {
 
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
@@ -24,10 +27,11 @@ const signup = async (req, res, next) => {
     );
   }
 
-  const { email, password } = req.body;
-  let existingUser
+  const {  email, password } = req.body;
+
+  let existingUser;
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
       'Signing up failed, please try again later.',
@@ -35,8 +39,8 @@ const signup = async (req, res, next) => {
     );
     return next(error);
   }
-  if (existingUser) {
 
+  if (existingUser) {
     const error = new HttpError(
       'User exists already, please login instead.',
       422
@@ -44,27 +48,59 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  const createdUser = new User({
-    email,
-    password,
-  });
+  let hashedPassword;
   try {
-    await createdUser.save();
+    hashedPassword = await bcrypt.hash(password, 12);
   } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again.',
+      'Could not create user, please try again.',
       500
     );
     return next(error);
   }
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+
+  const createdUser = new User({
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { email: createdUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
+
   let existingUser;
+
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
       'Logging in failed, please try again later.',
@@ -72,16 +108,52 @@ const login = async (req, res, next) => {
     );
     return next(error);
   }
-  if (!existingUser || existingUser.password !== password) {
+
+  if (!existingUser) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
-      401
+      403
     );
     return next(error);
   }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      403
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { email: existingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
   res.json({
-    message: 'Logged in!',
-    user: existingUser.toObject({ getters: true })
+    email: existingUser.email,
+    token: token
   });
 };
 
